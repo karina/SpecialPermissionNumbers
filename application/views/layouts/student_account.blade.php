7c431e05aa6cc5820b9f4a5f998ee54c84b08399
Karina Ruzinov <kruzinova@gmail.com>
2013-04-23 14:51:07 -0400

    added template for student account

---
+++ application/views/layouts/student_account.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>Your Account</title>
+    {{Asset::styles()}}
+  </head>
+
+  <body>
+    <div class="container">
+      
+      <nav class="top-bar">
+        <ul class="title-area">
+          <li class="name">
+            <h1><a href="/">Home</a></h1>
+          </li>
+        </ul>
+        
+        <section class="top-bar-section">
+          <ul class="left">
+            <li class="divider"></li>
+            <li class="has-dropdown">
+              <a href="/index.php/account/professor">Professor</a>
+              <ul class="dropdown">
+              </ul>
+            </li>
+            <li class="divider"></li>
+          </ul>
+          <ul class="left">
+            <li class="has-dropdown">
+              <a href="/index.php/account/student">Student</a>
+              <ul class="dropdown">
+                <li>
+                  <a href="/index.php/account/student/edit">Edit Account</a>
+                </li>
+              </ul>
+            </li>
+            <li class="divider"></li>
+          </ul>
+        </section>
+      
+      </nav>
+
+      {{ Form::open() }}
+        <fieldset>
+          <legend>@yield('legend')</legend>
+          
+          <div class="row">
+            <div class="large-6 columns">
+              {{ Form::label('netid','NetID') }}
+              {{ Form::text('netid') }}
+            </div>
+            
+            <div class="large-6 columns">
+              {{ Form::label('password','Password') }}
+              {{ Form::password('password') }}
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="large-6 columns">
+              {{ Form::label('email_addr','Email Address') }}
+              {{ Form::text('email_addr') }}
+            </div>
+            
+            <div class="large-6 columns">
+              {{ Form::label('password_confirmation','Password Confirmation') }}
+              {{ Form::password('password_confirmation') }}
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="large-6 columns">
+              {{ Form::label('ruid','RUID') }}
+              {{ Form::text('ruid') }}
+            </div>
+            
+            <div class="large-6 columns">
+              {{ Form::label('major','Major') }}
+              {{ Form::text('major') }}
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="large-4 columns">
+              {{ Form::label('gpa','GPA') }}
+              {{ Form::text('gpa') }}
+            </div>
+        
+            <div class="large-4 columns">
+              {{ Form::label('credits','Credits') }}
+              {{ Form::text('credits') }}
+            </div>
+        
+            <div class="large-4 columns">
+              {{ Form::label('grad_year','Graduation Year') }}
+              {{ Form::text('grad_year') }}
+            </div>
+          </div>
+          
+          <div class="row">
+            <div class="large-2 columns">
+              <input class="nice blue radius button student_submit" type="submit" value="Submit">
+            </div>
+          
+            <div class="large-2 columns">
+              <input class="nice blue radius button student_reset" type="reset">
+            </div>
+          </div>
+        </fieldset>
+      {{ Form::close() }}  
+    </div>
+  </body>
+</html>
