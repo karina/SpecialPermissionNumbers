b70322a88393053f9fba2d8e92be4bfc87a1ec7e
Karina Ruzinov <kruzinova@gmail.com>
2013-04-30 14:56:38 -0400

    prof header layout

---
+++ application/views/layouts/professor.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>Professor Settings</title>
+    {{Asset::styles()}}
+  </head>
+
+  <body>
+    <div class="container">
+      
+      <nav class="top-bar">
+        <ul class="title-area">
+          <li class="name">
+            <h1><a href="/">Home</a></h1>
+          </li>
+        </ul>
+        
+        <section class="top-bar-section">
+          <ul class="left">
+            <li class="divider"></li>
+            <li><a href="/index.php/special/addcourses">Add Courses</a></li>
+            <li class="divider"></li>
+            <li><a href="/index.php/special/getcourses">View Courses</a></li>
+            <li class="divider"></li>
+          </ul>
+
+          @if (Auth::check())
+          <ul class="right">
+            <li class="divider hide-for-small"></li>
+            <li><a href="#">{{ Auth::user()->net_id }}</a></li>
+            <li><a href="/index.php/account/logout">Logout</a></li>
+          </ul>
+          @endif
+        </section>
+      
+      </nav>
+
+      @yield('content')
+    </div>
+  </body>
+</html>
