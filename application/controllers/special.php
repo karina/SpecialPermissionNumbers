742a5ddd8e9fb2f2a96cc2d88c0b6e874658ae68
Karina Ruzinov <kruzinova@gmail.com>
2013-05-05 17:07:27 -0400

    requests model and controller updated

--- application/controllers/special.php
+++ application/controllers/special.php
@@ -2,10 +2,26 @@
 
 class  Special_Controller extends Base_Controller{
   public function get_request_sp(){
-      return View::make('special.request_sp');
+    
+    return View::make('special.request_sp');
   }
+
   public function post_request_sp(){
-  return Redirect::to('special/request_sp.php');
+    
+    $params = array(
+      "cid" => Input::get('coursenum'),
+      "net_id" => Student::where('net_id', '=', Auth::user()->net_id),
+      "first" => Input::get('sec1'),
+      "second" => Input::get('sec2'),
+      "third" => Input::get('sec3')
+    );
+
+    $request = new Request;
+
+    $request->fill($params);
+    $request->save();
+
+    return Redirect::to('special/request_sp.php');
   } 
   public function get_create_sp() {
       return View::make('special.create_sp');
