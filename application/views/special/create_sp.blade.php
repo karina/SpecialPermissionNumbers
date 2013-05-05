f6ede4dbd9d45641855c7f5bfbc4f0b35ca2936e
Emma Schwalbe <[emmaschwalbe@comcast.net]>
2013-05-05 03:03:25 -0400

    update create_sp

--- application/views/special/create_sp.blade.php
+++ application/views/special/create_sp.blade.php
@@ -3,39 +3,39 @@
 
   <form method="POST" action="/index.php/account/professor/create_sp">
     <fieldset>
-    <legend>Create a course special permission</legend>
+    <legend>Create a Course Special Permission</legend>
 
-    <div class="row">
-      <div class="large-6 columns">
-        <label> Course Number</label>
-        <input type="text" name="netid">
-      </div>
-    </div>
       <div class="row">
-       <div class= "large-6 colums">
-        <label> Course Section </label>
-        <input type="text" name="coursesec">
+           <div class="large-6 columns">
+           <label>Course Number</label>
+             <input type="text" name="coursenumber" id="coursenumber">
+           </div>
+          <div class="large-6 columns">
+            <label>Course Section</label>   
+            <input type="text" name="coursesection"  id="coursesection" >
+          </div>
       </div>
-    </div>
 
        <div class="row">
           <div class="large-6 columns">
            <label>Prerequesite 1</label>
-             <input type="text" name="prereq1">
+             <input type="text" name="pre1" id="pre1"  >
            </div>
           <div class="large-6 columns">
             <label>Prerequesite 2</label>   
-            <input type="text" name="prereq2">
+            <input type="text" name="pre2"  id="pre2"  >
           </div>
+        </div>
+        <div class="row">
           <div class="large-6 columns">
               <label>Prerequesite 3</label> 
-              <input type="text" name="prereq3">
+              <input type="text" name="pre3" id="pre3">
           </div>
          </div>
  <div class="row">
     <div class="large-6 columns">
-      <label>Special Permission number</label>
-      <input type="text" name="spnum">
+      <label>Special Permission Number</label>
+      <input type="text" name="spnum" id="spnum">
     </div>
 </div>
 
