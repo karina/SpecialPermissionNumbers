6976044abae2af49c377247bfb148e075f20b7c8
Leo Yu <leoyu037@gmail.com>
2013-05-05 21:00:58 -0400

    fixed foreign key in create courses migration

--- application/migrations/2013_04_28_062601_create_courses.php
+++ application/migrations/2013_04_28_062601_create_courses.php
@@ -15,10 +15,12 @@ class Create_Courses {
 			$table->integer('course_id');
 			$table->integer('sec_num')
 			
-			$table->string('net_id',64)->references('net_id')->on('professors');
+			$table->string('net_id',64);
 			$table->integer('max_students');
 			$table->integer('num_students');
 			$table->integer('room_num');		// simple implementation where course meets in only one room
+			
+			$table->foreign('net_id')->references('net_id')->on('professors');
 		});
 	}
 
