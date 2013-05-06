ebfe764fe79846fc595db9b1e35f42272e8c91df
Leo Yu <leoyu037@gmail.com>
2013-05-05 20:26:57 -0400

    fixed create_courses

--- application/migrations/2013_04_28_062601_create_courses.php
+++ application/migrations/2013_04_28_062601_create_courses.php
@@ -15,7 +15,7 @@ class Create_Courses {
 			$table->integer('course_id');
 			$table->integer('sec_num')
 			
-			$table->string('professor',64);
+			$table->string('net_id',64);
 			$table->integer('max_students');
 			$table->integer('num_students');
 			$table->integer('room_num');		// simple implementation where course meets in only one room
