0eb2de6f16b14e7cffb052bb7e652a420c8e94cf
Leo Yu <leoyu037@gmail.com>
2013-05-06 20:54:57 -0400

    fixed add special permission number view

--- application/views/special/create_sp.blade.php
+++ application/views/special/create_sp.blade.php
@@ -1,56 +1,40 @@
 @layout('layouts/professor')
 @section('content')
-
   <form method="POST" action="/index.php/account/professor/create_sp">
     <fieldset>
-    <legend>Create a Course Special Permission</legend>
+      <legend>Add Special Permission Number</legend>
+
+      <div class"row">
+        <div class "large-12 columns">
+          <p id="instructions">Add a special permission number here:</P>
+        </div>
+      </div>
 
       <div class="row">
-           <div class="large-6 columns">
-           <label>Course Number</label>
-             <input type="text" name="coursenumber" id="coursenumber">
-           </div>
-          <div class="large-6 columns">
-            <label>Course Section</label>   
-            <input type="text" name="coursesection"  id="coursesection" >
-          </div>
-      </div>
-
-       <div class="row">
-          <div class="large-6 columns">
-           <label>Prerequesite 1</label>
-             <input type="text" name="pre1" id="pre1"  >
-           </div>
-          <div class="large-6 columns">
-            <label>Prerequesite 2</label>   
-            <input type="text" name="pre2"  id="pre2"  >
-          </div>
+        <div class="large-6 columns">
+          <label>Course Number</label>
+          <input type="text" name="coursenumber" id="coursenumber">
         </div>
-        <div class="row">
-          <div class="large-6 columns">
-              <label>Prerequesite 3</label> 
-              <input type="text" name="pre3" id="pre3">
-          </div>
-         </div>
- <div class="row">
-    <div class="large-6 columns">
-      <label>Special Permission Number</label>
-      <input type="text" name="spnum" id="spnum">
-    </div>
-</div>
-
-
-
-    <div class="row">
-      <div class="large-6 columns">
-        <input class="nice blue radius button create_sp" type="submit" value="Submit">
+        <div class="large-6 columns">
+          <label>Course Section</label>   
+          <input type="text" name="coursesection" id="coursesection">
+        </div>
       </div>
-    </div>
 
-  </fieldset>
-</form>
-    
+      <div class="row">
+        <div class="large-6 columns">
+          <label>Special Permission Number</label>
+          <input type="text" name="spnum" id="spnum">
+        </div>
+      </div>
 
+      <div class="row">
+        <div class="large-6 columns">
+          <input class="nice blue radius button create_sp" type="submit" value="Submit">
+        </div>
+      </div>
 
+    </fieldset>
+  </form>
 
 @endsection
