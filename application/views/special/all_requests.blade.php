5512a0a29c092a3483a50cd0bb0df8d124dace9a
Emma Schwalbe <[emmaschwalbe@comcast.net]>
2013-05-07 19:46:22 -0400

    add edit and staus button

--- application/views/special/all_requests.blade.php
+++ application/views/special/all_requests.blade.php
@@ -2,12 +2,14 @@
 @section('content')
 
   @foreach($requests as $req) 
+<fieldset>
    <div class="requestblock">
        <h6> Course Number: {{ $req->course_id}} </h4>
             First Section Choice: {{ $req->first}} <br>
             Second Section Choice:{{ $req->second}}<br>
             Third Section Choice:{{ $req->third}}<br>
     </div>
+</fieldset>
     <br>
   @endforeach
 <form method="POST" action=/index.php/special/all_requests>
@@ -21,7 +23,7 @@
 
     <div class="row">
       <div class="large-6 columns">
-          <input class="nice blue radius button allrequests_submit">
+          <input class="nice blue radius button allrequests_submit" type="submit" value="drop"  >
       </div>
     </div>
 
--- application/views/special/prof_view_requests.blade.php
+++ application/views/special/prof_view_requests.blade.php
@@ -1,14 +1,23 @@
 @layout('layouts/student')
 @section('content')
 
-  @foreach($allrequests as $req) 
+  @foreach($allrequests as $req)
+<fieldset>
+ <div class="row">
    <div class="requestblock">
        <h6> Course Number: {{ $req->course_id}} </h4>
             First Section Choice: {{ $req->first}} <br>
             Second Section Choice:{{ $req->second}}<br>
             Third Section Choice:{{ $req->third}}<br>
             NET ID: {{ $req->net_id}}<br>
+        
+           <div class="large-3 coulmns">
+            <br> <input class="nice blue radius button prof_view_req_edit" type="submit" value="edit rank ">           </div>
+          <div class="large-3 coulmns">
+            <br> <input class="nice blue radius button prof_view_req_edit" type="submit" value="edit status">           </div>     
+        </div>
     </div>
+</fieldset>
     <br>
   @endforeach
 <form method="POST" action=/index.php/special/all_request >
@@ -22,7 +31,7 @@
 
   <div class="row">
     <div class="large-6 columns">
-      <input class="nice blue radius button prof_view_req_submit" type="submit" value="submit">
+      <input class="nice blue radius button prof_view_req_drop" type="submit" value="submit">
     </div>
   </div>
 
