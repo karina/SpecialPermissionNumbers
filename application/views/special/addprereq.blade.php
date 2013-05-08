7488676689118f388e425550d5b9ed7d1c254c59
Emma Schwalbe <[emmaschwalbe@comcast.net]>
2013-05-08 05:28:24 -0400

    add prereq and update controller

--- application/views/special/addprereq.blade.php
+++ application/views/special/addprereq.blade.php
@@ -1,58 +1,32 @@
 @layout('layouts/professor')
 @section('content')
 
-<script>
-function add_more_text_box(parent_id, child_id)
-{
-  var myTable=document.getElementById(parent_id);
-  var oDiv, oInput;
-  oDiv = document.createElement('div');
-  myTable.appendChild(oDiv);
 
-  oInput = document.createElement('input');
-  oInput.setAttribute('type', 'text');
-  oInput.setAttribute('name', child_id);
-  oDiv.appendChild(oInput);
-}
-var child_id = 1;
-function child()
-{
-  return child_id++;
-}
-</script>
-
-
- <form id="add_more" name="add_more" method="post" action="">
+<form method="POST" action="/index.php/special/addprereq">
   <fieldset>
- 
-    <div id="add_more_div">
-        <label>Add Prerequisite </label>
-         <div><input type="text" id="add_more" name="child_id" /></div>
-    </div>
-       <a href="javascript:;" onclick="return add_more_text_box('add_more_div',child());">Add Another Pre-req</a>
+        <div class="row">
+          <div class="large-6 columns">
+           @if (Session::has('error'))
+             <div class="alert alert-box">
+               {{ Session::get('error') }}
+               <a href="" class="close">&times;</a>
+             </div>
+           @endif
+          </div>
+        </div>
+        
+        
+           <label>Set Number </label>
+           <input type="text"  name="set_id">
+           <label>Course Number </label>
+           <input type="text"  name="course_id">
+           <label>Add Prerequisite </label>
+           <input type="text"  name="prereq">
+        
+         <input class="nice blue radius button submitprereq" type="submit" value"submit">
+
   </fieldset>
-
-    <input class="nice blue radius button addprereq_submit" type="submit" value="submit">
-
 </form> 
 
 @endsection
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--citation: -http://www.tutorialsscripts.com/free-java-scripts/dom/html-element/create-dynamic-multiple/add-more-text-box.php-->
